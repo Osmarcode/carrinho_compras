a16f0573f30e22070ab135e5be7c8a9093c375fe
osmarcode <osmarsiqueirasoares@gmail.com>
2024-08-12 23:27:31 -0300

    primeiro commit.

---
+++ src/carrinho_compras/CarrinhoDeCompras.java
@@ -0,0 +1,19 @@
+package carrinho_compras;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class CarrinhoDeCompras {
+    private List<Item> itens;
+
+    public CarrinhoDeCompras() {
+        this.itens = new ArrayList<>();
+    }
+
+    
+    // Adicionar item ao carrinho
+    public void 
+    
+    
+}
---
+++ src/carrinho_compras/Item.java
@@ -0,0 +1,43 @@
+package carrinho_compras;
+
+public class Item {
+	
+	private String nome;
+	private double preco;
+	private int quantidade;
+	
+	
+	
+	public Item(String nome, double preco, int quantidade) {
+		super();
+		this.nome = nome;
+		this.preco = preco;
+		this.quantidade = quantidade;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Item [nome=" + nome + ", preco=" + preco + ", quantidade=" + quantidade + "]";
+	}
+	
+	
+}
